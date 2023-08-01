bae0b5c2bc20ac9cb46b21d5fa8914988fa5b0cb
Sabari Raj <sabarinagsel004@gmail.com>
2023-08-01 11:53:45 +0530

    MiniProject

--- Mini Project/PayrollSystem/src/main/Display.java
+++
@@ -1,5 +0,0 @@
-package main;
-
-public class Display extends Printing {
-
-}
--- Mini Project/PayrollSystem/src/main/Print.java
+++
@@ -1,6 +0,0 @@
-package main;
-
-abstract interface Print{
-	abstract String print1 (String a);
-	abstract String print1(String a,int b);
-}
